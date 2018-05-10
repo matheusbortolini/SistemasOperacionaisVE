610863e3e990ea620903e42d8cfa92a71ff7d678
matheusbortolini <matheusbortolini@hotmail.com>
2018-05-10 13:30:54 -0300

    não funcionando

--- index.js
+++ index.js
@@ -17,6 +17,12 @@ fs.readFile('entrada.json', 'utf8', function(err, data){
     var countTempo = 0;
     //Colocar processos da entrada na fila
     for(processo of obj.filaQ0){
+        //Inicializar atributos que serão usado
+        processo.tempoEsperando = 0;
+        processo.tempoExecutando = 0;
+        processo.tempoFinal = 0;
+        processo.tempoRR = 0;
+        processo.filaOrigem = '';
         //Colocar o processo no fim da filaQ0
         filaQ0.push(processo);
     }
@@ -75,7 +81,7 @@ fs.readFile('entrada.json', 'utf8', function(err, data){
             filaQ0[0].tempoExecutando++;
             //Inclumentar o tempo executando RR em 1 unidade
             filaQ0[0].tempoRR++;
-            console.log(countTempo+" Processo:"+filaQ0[0].id+" execução total"+filaQ0[0].tempoExecutando+" RR "+filaQ0[0].tempoRR);
+            console.log(countTempo+" Processo:"+filaQ0[0].id+" Q0 execução total"+filaQ0[0].tempoExecutando+" RR "+filaQ0[0].tempoRR);
             //Verifica se o processo já rodou o suficiente para ir para filaIO
             if(filaQ0[0].tempoExecutando >= filaQ0[0].surtoCPU){
                 //Retira o processo da filaQ0
@@ -87,16 +93,19 @@ fs.readFile('entrada.json', 'utf8', function(err, data){
                 //Verificar se ainda existem operações de IO
                 if(processo.nrIO > 0){
                     //Coloca o processo na filaIO
+                    console.log(countTempo+" Processo:"+processo.id+" foi para IO depois Q0 de execução total"+processo.tempoExecutando+" RR "+processo.tempoRR);
                     filaIO.push(processo);
                 }//Caso não existam operações, processo terminou sua execução
                 else{
                     processo.tempoFinal = countTempo;
+                    console.log(countTempo+" Processo:"+processo.id+" finalizado em "+processo.tempoFinal);
                     filaFinalizado.push(processo);
                 }
             }//Verifica se o processo já rodou tempo suficiente no Round-Robin
             else if(filaQ0[0].tempoRR >= 10){
                 //Retirar o processo da filaQ0
                 processo = filaQ0.shift();
+                console.log(countTempo+" Processo:"+processo.id+" foi para Q1 depois de execução total"+processo.tempoExecutando+" RR "+processo.tempoRR);
                 //Zerar o tempoRR
                 processo.tempoRR = 0;
                 //Colocar o processo na filaQ1
@@ -108,6 +117,7 @@ fs.readFile('entrada.json', 'utf8', function(err, data){
             if(filaQ1){
                 //Rodar o primeiro processo por 1 ms (menor unidade de tempo)
                 filaQ1[0].tempoExecutando++;
+                console.log(countTempo+" Processo:"+filaQ1[0].id+" Q1 execução total"+filaQ1[0].tempoExecutando);
                 //Verifica se o processo já rodou o suficiente para ir para filaIO
                 if(filaQ1[0].tempoExecutando >= filaQ0[0].surtoCPU){
                     //Retira o processo da filaQ1
@@ -120,9 +130,11 @@ fs.readFile('entrada.json', 'utf8', function(err, data){
                     if(processo.nrIO > 0){
                         //Coloca o processo na filaIO
                         filaIO.push(processo);
+                        console.log(countTempo+" Processo:"+processo.id+" foi para IO depois de Q1 execução total"+processo.tempoExecutando);
                     }//Caso não existam operações, processo terminou sua execução
                     else{
                         processo.tempoFinal = countTempo;
+                        console.log(countTempo+" Processo:"+processo.id+" finalizado em "+processo.tempoFinal);
                         filaFinalizado.push(processo);
                     }
                 }
